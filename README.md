4211c5e688cf61cfadfa6aa4fa35ec221218ce5a
ddomenico <ddomenico96@gmail.com>
2021-06-10 16:53:26 -0400

    🚀 initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# Code accompanying *Feasibility and clinical utility of whole genome profiling in pediatric and young adult cancers*
+
+*Shukla N, Levine M F, Gundem G, et al.*
+
+## :open_file_folder: Repository Structure
+
+- **`data/`**: this directory contains the dataframes necessary to reproduce the results of the study.
+
+- **`notebooks/`**: this directory contains notebooks for generating figures and dataframes.
+
+
+## :rocket: Getting started with this repository
+
+### Clone the repository
+
+To clone this repository on your local computer please run:
+
+```shell
+$ git clone https://github.com/papaemmelab/Shukla_Levine_Gundem
+```
+
+Install with pip.
+
+    cd Shukla_Levine_Gundem
+    pip install -r requirements.txt
---
+++ setup.py
@@ -0,0 +1,30 @@
+"""Shukla_Levine_Gundem setup.py."""
+
+import json
+import subprocess
+from os.path import abspath, dirname, join
+
+from setuptools import find_packages, setup
+
+ROOT = abspath(dirname(__file__))
+
+# please put setup keywords in the setup.json to keep this file clean
+with open(join(ROOT, "setup.json"), "r") as f:
+    SETUP = json.load(f)
+
+# see 4 > https://packaging.python.org/guides/single-sourcing-package-version/
+with open(join(ROOT, "VERSION"), "r") as f:
+    VERSION = f.read().strip()
+
+setup(
+    # single source package version
+    version=VERSION,
+    # in combination with recursive-includes in MANIFEST.in, non-python files
+    # within the click_egreport will be copied into the
+    # site-packages and wheels installation directories
+    include_package_data=True,
+    # return a list all Python packages found within the ROOT directory
+    packages=find_packages(),
+    # pass parameters loaded from setup.json including author and version
+    **SETUP,
+)
